c816cbcf7c120914dab54cb8d683a24390b8e0a9
Yauhen Shviatsou <demum1930@gmail.com>
2022-06-28 18:04:51 +0300

    Fix: refactoring

--- src/test/java/SeleniumDemoTest.java
+++ src/test/java/SeleniumDemoTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -18,25 +19,27 @@ public class SeleniumDemoTest {
     }
 
     @Test
-  public  void getTitleInOpenedsiteVerifyWithText() {
+    public void getTitleInOpenedsiteVerifyWithText() {
 
         System.out.println("In verify title test case");
         String title = driver.getTitle();
         assertTrue(title.contains("Online Tutorials Library"));
 
     }
-        @Test
-        public void getHeadingVerifyWithText () {
-            System.out.println("In verify Heading Text Above Search Test Case");
-            String expectedSearchHeading = "Simply Easy Learning at your fingertips";
-            String actualHeading = driver.findElement(By.xpath("//h1")).getText();
-assertEquals(expectedSearchHeading,actualHeading);
 
-        }
-        @AfterClass
-        public static void afterWorkCloseDriver() {
-            driver.quit();
-        }
+    @Test
+    public void getHeadingVerifyWithText() {
+        System.out.println("In verify Heading Text Above Search Test Case");
+        String expectedSearchHeading = "Simply Easy Learning at your fingertips";
+        String actualHeading = driver.findElement(By.xpath("//h1")).getText();
+        assertEquals(expectedSearchHeading, actualHeading);
 
     }
+
+    @AfterClass
+    public static void afterWorkCloseDriver() {
+        driver.quit();
+    }
+
+}
 
